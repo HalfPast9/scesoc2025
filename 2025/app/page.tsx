646265ab22dd87045f6e12ef0a292fc24049f4f0
Shri Vempati <shrikar.exe@gmail.com>
2025-03-31 14:37:10 -0400

    landingpage

--- 2025/app/page.tsx
+++ 2025/app/page.tsx
@@ -2,16 +2,19 @@
 
 import { MainNav } from "@/components/main-nav"
 import { HeroCarousel } from "@/components/hero-carousel"
-
+import { HeroSectionWithBeamsAndGrid } from "@/components/HeroSectionWithBeamsAndGrid";
+import { SimpleNavbarWithHoverEffects } from "@/components/SimpleNavbarWithHoverEffects";
 
 export default function Home() {
   return (
     <div className="dark min-h-screen bg-background">
       <header className="container mx-auto px-4">
-        <MainNav />
+        <SimpleNavbarWithHoverEffects />
       </header>
 
       <main>
+      	<SimpleNavbarWithHoverEffects />
+      	<HeroSectionWithBeamsAndGrid />
         <HeroCarousel />
         {/* <UpcomingEvents /> */}
         {/* <AboutSection /> */}
