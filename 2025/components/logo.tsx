c917ab47b6990e5f99fa730e2c20e2fe06e9e22e
Shri Vempati <shrikar.exe@gmail.com>
2025-03-31 15:13:44 -0400

    newlogosetup

--- 2025/components/logo.tsx
+++ 2025/components/logo.tsx
@@ -1,19 +1,7 @@
 "use client";
-import { cn } from "@/lib/utils";
+import Image from "next/image";
 import Link from "next/link";
-
-const LogoIcon = () => (
-  <div
-    className={cn(
-      "w-8 h-8 bg-black border border-neutral-400 rounded-full",
-      "inline-flex items-center justify-center border border-solid", // Changed to flex for alignment
-      "[border-image-source:linear-gradient(180deg,#1F1F1F_0%,#858585_100%),linear-gradient(180deg,#1F1F1F_0%,#858585_100%)]",
-      "[background:linear-gradient(0deg,#151515,#151515),linear-gradient(180deg,rgba(21,21,21,0)_66.3%,rgba(255,255,255,0.5)_100%),linear-gradient(183.22deg,rgba(255,255,255,0.5)_2.62%,rgba(21,21,21,0)_52.03%)]",
-      "shadow-[inset_0px_6px_8px_0px_#FAFAFA40,inset_0px_-6px_8px_0px_#FAFAFA40,0px_0px_0px_0px_#FAFAFA40,0px_0px_0px_0px_#FAFAFA40]",
-      "text-white"
-    )}
-  ></div>
-);
+import { cn } from "@/lib/utils";
 
 export const Logo = () => {
   return (
@@ -21,9 +9,16 @@ export const Logo = () => {
       href="/"
       className="font-normal flex gap-2 items-center text-sm text-black px-2 py-1 shrink-0 relative z-20"
     >
-      <LogoIcon />
-
-      <span className="font-medium text-white">CryptGen</span>
+      <Image
+        src="SCESoc_Logo_Yellow_White_Horizontal.png"
+        alt="SCESoc Logo"
+        width={40}
+        height={40}
+        className="rounded-md"
+      />
+      <span className="font-medium text-white">SCESoc</span>
     </Link>
   );
 };
+
+
